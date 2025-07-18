b44fee0a69eb57f4e5c8104cb2f5342a83e22151
marco <marcoadrianzambra@gmail.com>
2025-07-17 23:23:44 -0500

    feat: vite routes, src, components and pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // <--- ¡Asegúrate de que esta línea esté aquí!
 
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    }
+  },
+});
